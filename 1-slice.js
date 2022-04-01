ecd6f9d69009459aab8f9b40cc2b4ffb96f495b9
ANDRIATIANA <98638468+andriatiana12@users.noreply.github.com>
2022-04-01 17:08:45 +0300

    commentary

--- 1-slice.js
+++ 1-slice.js
@@ -1,9 +1,16 @@
 function slice(array, start, end) {
-  let slicedArray = [];
-  for (let i = start; i < end; i++) {
-    slicedArray.push(array[i]);
+  let slicedArray = [];1
+  for (let i = start; i < end; i++) { /*5*/
+    slicedArray.push(array[i]);/*2*/
   }
-  return slicedArray;
+  return slicedArray; /*1*/
 }
 
 module.exports = { slice };
+
+/*
+o(n) = 2 + 5(n)*(2 )+ 1 = 2 + 5(n) + 1 
+
+
+O(n) = complexite  lineaire 
+*/
