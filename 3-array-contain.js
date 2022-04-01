4c69d03498080aaff3917d86772305227f268ea1
ANDRIATIANA <98638468+andriatiana12@users.noreply.github.com>
2022-04-01 17:09:54 +0300

    Update 3-array-contain.js

--- 3-array-contain.js
+++ 3-array-contain.js
@@ -1,10 +1,12 @@
 function arrayContain(array, number) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === number) {
-      return true;
+  for (let i = 0; i < array.length; i++) { //4
+    if (array[i] === number) {//3
+      return true;//1
     }
   }
-  return false;
+  return false;//1
 }
 
 module.exports = { arrayContain };
+
+ /*O(n) = complexité logarithmique */
