6b7d3559a0d5e3fbbb9bc1677958789e52221ad2
ANDRIATIANA <98638468+andriatiana12@users.noreply.github.com>
2022-04-01 17:09:27 +0300

    Update 2-merge.js

--- 2-merge.js
+++ 2-merge.js
@@ -1,13 +1,13 @@
 function merge(array1, array2) {
-  let mergedArray = [];
-  let i = 0;
-  let j = 0;
-  while (i < array1.length && j < array2.length) {
-    if (array1[i] < array2[j]) {
-      mergedArray.push(array1[i]);
+  let mergedArray = [];/*1*/
+  let i = 0;/*1*/
+  let j = 0;//1 
+  while (i < array1.length && j < array2.length) { //5
+    if (array1[i] < array2[j]) {2
+      mergedArray.push(array1[i]);// 3
       i++;
     } else {
-      mergedArray.push(array2[j]);
+      mergedArray.push(array2[j]);//3
       j++;
     }
   }
@@ -19,7 +19,11 @@ function merge(array1, array2) {
     mergedArray.push(array2[j]);
     j++;
   }
-  return mergedArray;
+  return mergedArray;1
 }
 
 module.exports = { merge };
+ /*
+O(n) = 1 + 5(n)*(2+1+3) +  1  = 
+
+O(n) = complexité lineaire */
